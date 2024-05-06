f23e79c872b0ae015797972a07bfbc365175b38c
Hayssam Al Mawla <hayssam@rudr-tech.com>
2024-05-07 00:08:06 +0300

    init

---
+++ admin/manage courses/add courses/add_course_action.php
@@ -0,0 +1,20 @@
+<?php
+    require_once '../../../connection/db_connection.php';
+
+    session_start();
+    if (!isset($_SESSION['username']) || !isset($_SESSION['password'])) {
+        header("Location: login.php"); 
+        exit();
+    }
+
+    $insertStmt = $mysqli->prepare("INSERT INTO courses (CourseName, CourseDescription, CreditHours) VALUES (?, ?, ?)");
+    $insertStmt->bind_param("sss", $_POST['CourseName'],$_POST['CourseDescription'], $_POST['CreditHours']);
+    $insertStmt->execute();
+
+    if ($insertStmt->affected_rows > 0) {
+        echo json_encode(array("success" => "New record created successfully"));
+    } else {
+        echo json_encode(array("error" => "Error: " . $mysqli->error));
+    }
+
+    ?>
